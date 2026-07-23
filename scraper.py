74f03b5ec53f909123e0f319522dc8252a7c9ddb
leoof7 <54754281+leoof7@users.noreply.github.com>
2026-07-23 04:29:46 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ from playwright.sync_api import sync_playwright
 # CONFIGURAÇÕES DA INTEGRAÇÃO
 # ==========================================
 # ⚠️ ATENÇÃO: Cole a URL final do Apps Script que termina em /exec
-WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbztJ0eU-OajlwQKL7VC7qSzpGjQ1375U5exEdNx49c4TAcA-If4NnFI1OryoDaOHR9REw/exec"
+WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbw6Zu27L5c9wzVFz-pi2EkgAvZmd5JOexqMIfDWFK_hQTDxwIXjspcS7zv7YxfT8jNcdw/exec"
 
 TOKEN = "8f88b4c964"
 DIAS_PARA_RASPAR = ["ontem", "hoje", "amanha"]
